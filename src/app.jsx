ce076b5f0aeac78d5e4e217f70b9f340ab79d6a3
Mykyta Meshcheriakov <6etmah@gmail.com>
2017-11-01 21:41:38 +0200

    header moved to main app

--- src/app.jsx
+++ src/app.jsx
@@ -4,11 +4,18 @@ import { Route, Switch } from 'react-router-dom';
 
 import MainPageContainer from './components/containers/MainPageContainer.jsx';
 import AccountContainer from './components/containers/AccountContainer.jsx';
+import SellAccountContainer from './components/containers/SellAccountContainer.jsx';
+import Header from './components/Header.jsx';
 
-const App = () =>
-	<Switch>
-		<Route exact path="/" component={MainPageContainer} />
-		<Route path="/account/:id" component={AccountContainer} />
-	</Switch>;
+const App = () => (
+	<div>
+		<Header />
+		<Switch>
+			<Route exact path="/" component={MainPageContainer} />
+			<Route path="/account/:id" component={AccountContainer} />
+			<Route path="/sell" component={SellAccountContainer} />
+		</Switch>
+	</div>
+);
 
 export default App;
